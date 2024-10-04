4d2f0bd825090bfc2a2ce7d2707bcf2234d670da
Harshit Mogha <moghaeashu@gmail.com>
2024-10-04 23:13:54 +0530

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,12 @@
+gsap.to("#about-2-one-1 img",{
+    transform:"translateX(-1000%)",
+    duration:40,
+    repeat:-1,
+    yoyo:"true",
+})
+gsap.to("#about-2-one-2 img",{
+    transform:"translateX(-1000%)",
+    duration:40,
+    repeat:-1,
+    yoyo:"true",
+})
